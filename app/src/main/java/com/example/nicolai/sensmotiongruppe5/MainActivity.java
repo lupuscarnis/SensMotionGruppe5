d8758ac56a7434b1adf42d4bc02605ed5654ada4
Alan <s122417@student.dtu.dk>
2018-10-01 10:59:55 +0200

    Alan

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Testing for github
         //Hej fra Nick
+        //Hej fra Mr. Alan
     }
 }
