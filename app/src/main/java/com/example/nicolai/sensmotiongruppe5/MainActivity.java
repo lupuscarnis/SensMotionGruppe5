e04790050796470f861ac2c9faf2bb7844223a00
Darthlatte <savethecat@gmail.com>
2018-10-16 23:44:59 +0200

    Made signin acitivity the default onCreate()

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
@@ -8,7 +8,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        
+        // Testing login
+        setContentView(R.layout.activity_login);
         //testing for branch github
     }
 }
