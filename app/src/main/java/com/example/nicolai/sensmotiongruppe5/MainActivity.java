bd8fd7883c7b6c9a4030d1cb4009a6feec248a51
Nicolai Barnett <lupuscarnis@gmail.com>
2018-10-28 08:35:36 +0100

    Merge branches '1.-Iteration' and 'master' of https://github.com/lupuscarnis/SensMotionGruppe5 into 1.-Iteration
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
    And removed comments from main

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Testing for github
-        //Hej fra Nick
-        //Hej fra Mr. Alan
-        //Hej fra Kasper
+        
     }
 }
