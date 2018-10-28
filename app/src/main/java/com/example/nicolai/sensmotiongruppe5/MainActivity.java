cb369fca0c653e127cf96148e260edde13c88273
Darthlatte <savethecat@gmail.com>
2018-10-28 09:34:19 +0100

    Redesign of login activity

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
@@ -8,10 +8,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_main);
         //Testing for github
         //Hej fra Nick
         //Hej fra Mr. Alan
         //Hej fra Kasper
+=======
+        //setContentView(R.layout.activity_main);
+
+        // Testing login
+        setContentView(R.layout.activity_login);
+        //testing for branch github
+>>>>>>> Stashed changes
     }
 }
