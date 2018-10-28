08978351590beb584bb4e07be97ee950d3db9417
Nicolai Barnett <lupuscarnis@gmail.com>
2018-10-28 09:02:32 +0100

    Added the handles for the buttons in the java class

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Help.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Help.java
@@ -1,13 +1,46 @@
 package com.example.nicolai.sensmotiongruppe5;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class Help extends AppCompatActivity {
+public class Help extends AppCompatActivity implements View.OnClickListener {
+
+    TextView writtenText ;
+    ImageButton play,pause,read;
+    Button next;
+    MediaPlayer mp;
+    EditText input;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        next = findViewById(R.id.help_next);
+        read = findViewById(R.id.help_read);
+        writtenText = findViewById(R.id.help_textbox);
+        input = findViewById(R.id.help_input);
+        
+
+
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
