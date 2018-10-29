2c65e7d2038392238a96e9f3bc9d2187f4ce9eaa
Darthlatte <savethecat@gmail.com>
2018-10-30 00:31:47 +0100

    Created an intent to the login activity from mainactivity. User can login with "@" in the password input field

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nicolai.sensmotiongruppe5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Launch Login Activity. Use "@" in email to get past it.
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
 
         //Alan
 
