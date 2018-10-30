c9373a2da47e2300dc33afd02f065ec410a885c1
Kasper Valente <savethecat@gmail.com>
2018-10-30 11:06:24 +0100

    Changed default activity from main to forside

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/LoginActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.nicolai.sensmotiongruppe5;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -301,6 +302,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
         }
+    }
+
+    public void helped(View v){
+        Intent i = new Intent(this, Help.class);
+
+
+
+        startActivity(i);
+
+
     }
 }
 
