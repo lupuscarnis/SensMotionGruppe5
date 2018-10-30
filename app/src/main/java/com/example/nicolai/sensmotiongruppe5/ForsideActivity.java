d5c0ec683c89afeda658a3d2c436ea7f964f1f20
Alan <s122417@student.dtu.dk>
2018-10-30 15:05:57 +0100

    more SettingActivity

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/ForsideActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/ForsideActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class ForsideActivity extends AppCompatActivity {
+
+    ImageButton indstil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +17,26 @@ public class ForsideActivity extends AppCompatActivity {
 
         // Launch Login Activity. Use "@" in email to get past it.
         startActivity(new Intent(ForsideActivity.this, LoginActivity.class));
-        //Alan
 
+
+        indstil = findViewById(R.id.forside_ind);
+        indstil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettingActivity();
+            }
+        });
+    }
+
+    public void openSettingActivity(){
+        Intent intent = new Intent(this, SettingActivity.class);
+                startActivity(intent);
     }
 
 
 
 public void helped(View v){
      Intent   i = new Intent(this, HelpActivity.class);
-
-
 
  startActivity(i);
 
