d3130f402ba00c65c8dfb0f7e6b07f05b3194707
Nicolai Barnett <lupuscarnis@gmail.com>
2018-10-31 09:37:40 +0100

    added a new Sound file added some more text in strings removed activitys for password made is so username does it all and finishes help if it gets stopped.

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Help_Username_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Help_Username_Activity.java
@@ -14,15 +14,12 @@ import android.content.Intent;
 
 public class Help_Username_Activity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener {
 
-    private TextView writtenText ;
-    private ImageButton play,pause,read;
-    private Button next;
+    private TextView writtenText;
+    private ImageButton play, pause, read;
+    private Button next, help_return;
     private MediaPlayer mp;
     private EditText input;
     private ImageButton setting;
-
-
-
 
 
     @Override
@@ -31,21 +28,24 @@ public class Help_Username_Activity extends AppCompatActivity implements View.On
         setContentView(R.layout.activity_help_username);
 
         // Finds all the Elements that can be manipulateted
+        help_return = findViewById(R.id.help_return);
         next = findViewById(R.id.help_next);
         read = findViewById(R.id.help_read);
         pause = findViewById(R.id.help_pause);
         play = findViewById(R.id.help_play);
         writtenText = findViewById(R.id.help_textbox);
         input = findViewById(R.id.help_input);
-        mp =  MediaPlayer.create(getApplicationContext(), R.raw.bruger_id);
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.bruger_id);
         setting = findViewById(R.id.indstillinger);
 
         //Sets the Text on screen
         writtenText.setText(R.string.text_user_login_help);
         next.setText(R.string.text_user_login_help_button);
+        help_return.setText(R.string.text_user_return_button);
 
 
         //Sets all the Listnerens For this activity
+        help_return.setOnClickListener(this);
         read.setOnClickListener(this);
         pause.setOnClickListener(this);
         play.setOnClickListener(this);
@@ -54,82 +54,99 @@ public class Help_Username_Activity extends AppCompatActivity implements View.On
         mp.setOnCompletionListener(this);
 
 
-
     }
+
     // Releases the MediaPlayer
     @Override
     protected void onStop() {
         super.onStop();
-        if(mp != null){
+
+        if (mp != null) {
             mp.release();
         }
+        this.finish(); 
 
     }
+
     // Releases the  MediaPlayer
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
-        if(mp != null){
+        if (mp != null) {
             mp.release();
         }
 
 
-
     }
-
 
 
     @Override
     public void onClick(View v) {
         // enters the settings menu
-        if(v == setting)
-        {
-            Intent i = new Intent(Help_Username_Activity.this,SettingActivity.class);
+        if (v == setting) {
+            Intent i = new Intent(Help_Username_Activity.this, SettingActivity.class);
             startActivity(i);
-         }
-         // goes to the next screen or returns to login
-        if(v == next){
+        }
+        // goes to the next screen or returns to login
+        if (v == next) {
+            if (input.getText().toString().isEmpty()) {
+                input.setHint("Du skal skrive et brugernavn");
+            } else {
+                if(mp != null)
+                {
+                    mp.release();
+                }
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.plan_with_man);
+                writtenText.setText(R.string.text_user_password);
+                //input.getText().toString();
+                input.setText("", TextView.BufferType.EDITABLE);
+                input.setHint("Password");
+                next.setVisibility(View.INVISIBLE);
+                help_return.setVisibility(View.VISIBLE);
+
+            }
+        }
+        //After password has been inserted return.
+        if (v == help_return) {
             Intent i = new Intent(Help_Username_Activity.this, LoginActivity.class);
             startActivity(i);
-            }
-    //starts the Mp3 file
-        if(v == read){
+
+        }
+
+
+        //starts the Mp3 file
+        if (v == read) {
             read.setVisibility(View.INVISIBLE);
             pause.setVisibility(View.VISIBLE);
             mp.start();
 
 
-
         }
         //pause the MP3 file
-        if(v == pause)
-        {
+        if (v == pause) {
             play.setVisibility(View.VISIBLE);
             pause.setVisibility(View.INVISIBLE);
             mp.pause();
         }
         // Resumes the MP3 song
-        if(v == play)
-        {
+        if (v == play) {
             mp.start();
             play.setVisibility(View.INVISIBLE);
             pause.setVisibility(View.VISIBLE);
 
         }
-        
-
-
-
-    }
-
-    public void onCompletion(MediaPlayer arg0)
-    {
-
-        read.setVisibility(View.VISIBLE);
-        pause.setVisibility(View.INVISIBLE);
-        play.setVisibility(View.INVISIBLE);
 
 
     }
 
-}
+        public void onCompletion (MediaPlayer arg0)
+        {
+
+            read.setVisibility(View.VISIBLE);
+            pause.setVisibility(View.INVISIBLE);
+            play.setVisibility(View.INVISIBLE);
+
+
+        }
+
+    }
