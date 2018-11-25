8e957509218b0cc7db42d3af1d0791df859074ee
Darthlatte <savethecat@gmail.com>
2018-11-25 03:17:35 +0100

    - Added DAO (Singleton) - Added RESTConnection (For downloading JSON) - Added new package BLL (Business Logic Layer)

---
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/BLL/DAO.java
@@ -0,0 +1,40 @@
+package com.example.nicolai.sensmotiongruppe5.BLL;
+
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DAO  {
+
+    private static final String TAG = DAO.class.getSimpleName();
+    ArrayList<HashMap<String, String>> dataList;
+
+    public String patient_key;
+    public String project_key;
+
+    // Singleton
+    private static DAO instance ;
+
+    private DAO() {}
+
+    public static DAO getInstance() {
+
+        if (instance == null) {
+            instance = new DAO();
+        }
+
+        return instance;
+
+    }
+
+    public void getData(){
+
+        JSONConnection jsonConnection = new JSONConnection();
+        // Making a request to url and getting response
+        String url = "https://beta.sens.dk/exapi/1.0/patients/data/external/overview?project_key=k5W2uX&patient_key=6rT39u&day_count=7";
+        String jsonStr = jsonConnection.getJSON(url);
+        Log.e(TAG, "Response from url: " + jsonStr);
+
+    }
+
+}
--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/ForsideActivity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/ForsideActivity.java
@@ -1,19 +1,28 @@
 package com.example.nicolai.sensmotiongruppe5;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.nicolai.sensmotiongruppe5.BLL.DAO;
 
 public class ForsideActivity extends AppCompatActivity {
 
     ImageButton indstil;
 
+    // DAO singleton instance object
+    DAO userDAO = DAO.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forside);
+
+        new GetJSON().execute();
 
         indstil = findViewById(R.id.forside_ind);
         indstil.setOnClickListener(new View.OnClickListener() {
@@ -29,9 +38,32 @@ public class ForsideActivity extends AppCompatActivity {
                 startActivity(intent);
     }
 
+    /**
+     * Download JSON as ASYNCTASK
+     * Prompts to screen
+     */
+
+    private class GetJSON extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+            Toast.makeText(ForsideActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+
+        }
+
+        protected Void doInBackground(Void... arg0) {
+
+            userDAO.getData();
+            return null;
+
+        }
+    }
 
 
-public void helped(View v){
+
+    public void helped(View v){
      Intent   i = new Intent(this, Help_Activity.class);
 
  startActivity(i);
