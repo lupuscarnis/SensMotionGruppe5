5b67333b5a6013f301ec0b08653a4725f6facd71
Nicolai Barnett <lupuscarnis@gmail.com>
2018-11-26 18:06:24 +0100

    I have implemetned the View Pager with the Pie Chart but cannot get the line graf to show. will ask alan tomorrow

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Fragments/Min_Data_frag.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Fragments/Min_Data_frag.java
@@ -1,7 +1,6 @@
 package com.example.nicolai.sensmotiongruppe5.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,20 +9,28 @@ import android.view.ViewGroup;
 
 import com.example.nicolai.sensmotiongruppe5.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
+
 
 public class Min_Data_frag extends Fragment {
-
-
+    public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+    private PieChartView pieChartView;
 
     public Min_Data_frag() {
 
     }
 
 
-    public static Min_Data_frag newInstance() {
+    public static Min_Data_frag newInstance(String message) {
         Min_Data_frag fragment = new Min_Data_frag();
         Bundle args = new Bundle();
-
+        args.putString(EXTRA_MESSAGE, message);
+        fragment.setArguments(args);
         return fragment;
     }
 
@@ -37,8 +44,32 @@ public class Min_Data_frag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_min__data_frag, container, false);
+
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        pieChartView = getView().findViewById(R.id.pieChart);
+
+        List<SliceValue> pieData = new ArrayList<>();
+
+
+        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
+        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
+        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
+        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
+
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasLabels(true);
+        pieChartView.setPieChartData(pieChartData);
+
+
     }
 
 
--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Data_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Data_Activity.java
@@ -1,9 +1,11 @@
 package com.example.nicolai.sensmotiongruppe5;
 
-import android.support.v4.app.FragmentTransaction;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import com.example.nicolai.sensmotiongruppe5.Fragments.Date_Slider_frag;
 import com.example.nicolai.sensmotiongruppe5.Fragments.Min_Data_frag;
@@ -11,41 +13,78 @@ import com.example.nicolai.sensmotiongruppe5.Fragments.Min_Data_frag;
 import java.util.ArrayList;
 import java.util.List;
 
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.SliceValue;
-import lecho.lib.hellocharts.view.PieChartView;
 
-public class Min_Data_Activity extends AppCompatActivity {
+public class Min_Data_Activity extends FragmentActivity {
+
+    MyPageAdapter pageAdapter;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_min__data);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.place_holder_date, Date_Slider_frag.newInstance());
-        ft.commit();
+        List<Fragment> fragments = getFragments();
 
 
-        FragmentTransaction fb = getSupportFragmentManager().beginTransaction();
-        fb.replace(R.id.place_holder_graf, Min_Data_frag.newInstance());
-        fb.commit();
-        PieChartView pieChartView = findViewById(R.id.chart);
+        pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
 
-        List<SliceValue> pieData = new ArrayList<>();
+        ViewPager pager = findViewById(R.id.min_data_fragment_pager);
 
 
+        pager.setAdapter(pageAdapter);
 
 
-        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
-        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
-        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
-        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
-
-
-
-
-        PieChartData pieChartData = new PieChartData(pieData);
-        pieChartData.setHasLabels(true);
-        pieChartView.setPieChartData(pieChartData);
     }
+
+    private List<Fragment> getFragments() {
+
+        List<Fragment> fList = new ArrayList<>();
+
+
+        fList.add(Min_Data_frag.newInstance("Fragment 1"));
+        fList.add(Date_Slider_frag.newInstance("Fragment 2"));
+
+
+        return fList;
+
+    }
+
+
 }
+
+class MyPageAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+
+    public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
+
+        super(fm);
+
+        this.fragments = fragments;
+
+    }
+
+    @Override
+
+    public Fragment getItem(int position) {
+
+        return this.fragments.get(position);
+
+    }
+
+
+    @Override
+
+    public int getCount() {
+
+        return this.fragments.size();
+
+    }
+
+
+}
+
