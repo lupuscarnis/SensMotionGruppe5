c30ee182955da5848248fa9a717e62ba92e10e0a
Alan <s122417@student.dtu.dk>
2018-11-26 10:41:04 +0100

    PieCHart Added.

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Data_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Data_Activity.java
@@ -1,7 +1,15 @@
 package com.example.nicolai.sensmotiongruppe5;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class Min_Data_Activity extends AppCompatActivity {
 
@@ -9,5 +17,23 @@ public class Min_Data_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_min__data);
+        PieChartView pieChartView = findViewById(R.id.chart);
+
+        List<SliceValue> pieData = new ArrayList<>();
+
+
+
+
+        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
+        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
+        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
+        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
+
+
+
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasLabels(true);
+        pieChartView.setPieChartData(pieChartData);
     }
 }
