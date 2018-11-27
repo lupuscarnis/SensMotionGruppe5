a663d80b94105421b996b78d8a69e830be0788a6
Sophus <sophusweirsoe@hotmail.dk>
2018-11-27 15:17:41 +0100

    to be deleted

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Side_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Side_Activity.java
@@ -3,15 +3,20 @@ package com.example.nicolai.sensmotiongruppe5;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.nicolai.sensmotiongruppe5.BLL.DAO;
 
-public class Min_Side_Activity extends AppCompatActivity {
-
+public class Min_Side_Activity extends AppCompatActivity  {
     ImageButton indstil;
 
     // DAO singleton instance object
@@ -20,7 +25,18 @@ public class Min_Side_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forside);
+        setContentView(R.layout.navigation_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         new GetJSON().execute();
 
@@ -32,12 +48,43 @@ public class Min_Side_Activity extends AppCompatActivity {
             }
         });
     }
-
     public void openSettingActivity(){
         Intent intent = new Intent(this, Setting_Activity.class);
                 startActivity(intent);
     }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
     /**
      * Download JSON as ASYNCTASK
      * Prompts to screen
@@ -60,9 +107,6 @@ public class Min_Side_Activity extends AppCompatActivity {
 
         }
     }
-
-
-
     public void helped(View v){
      Intent   i = new Intent(this, Help_Activity.class);
 
