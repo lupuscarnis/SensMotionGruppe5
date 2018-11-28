fdc69c4098928cfd64dc510b438fe1d6fe4acbeb
Sophus <sophusweirsoe@hotmail.dk>
2018-11-28 11:21:05 +0100

    Navigation drawer (side menu)

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Side_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Side_Activity.java
@@ -3,7 +3,13 @@ package com.example.nicolai.sensmotiongruppe5;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -18,30 +24,91 @@ public class Min_Side_Activity extends AppCompatActivity implements View.OnClick
     // DAO singleton instance object
     DAO userDAO = DAO.getInstance();
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forside);
+        setContentView(R.layout.navigation_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+
+
+
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    // set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // close drawer when item is tapped
+                    mDrawerLayout.closeDrawers();
+
+                    // Add code here to update the UI based on the item selected
+                    // For example, swap UI fragments here
+                    return true;
+                }});
+
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }});
+
 
         new GetJSON().execute();
         help = findViewById(R.id.Min_side_help);
         help.setOnClickListener(this);
-        indstil = findViewById(R.id.forside_ind);
-        indstil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSettingActivity();
-            }
-        });
+
 
     }
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item){
+                switch (item.getItemId()) {
+                    case android.R.id.home:
+                        mDrawerLayout.openDrawer(GravityCompat.START);
+                        return true;
+                }
+                return super.onOptionsItemSelected(item);
+
+            }
+
+
+
+
+
+
+
+
 
     public void openSettingActivity(){
         Intent intent = new Intent(this, Setting_Activity.class);
                 startActivity(intent);
     }
 
-    /**
+    /*
      * Download JSON as ASYNCTASK
      * Prompts to screen
      */
