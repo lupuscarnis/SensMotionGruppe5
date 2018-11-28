3c0377055d20ac728cd5941df7ffe13bae77e7b2
lupuscarnis <32562715+lupuscarnis@users.noreply.github.com>
2018-11-28 16:08:50 +0100

    Added the Seekbar with Ranges but we need to get som data

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Data_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Min_Data_Activity.java
@@ -1,5 +1,6 @@
 package com.example.nicolai.sensmotiongruppe5;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -10,6 +11,7 @@ import android.support.v4.view.ViewPager;
 
 import com.example.nicolai.sensmotiongruppe5.Fragments.Line_chart_frag;
 import com.example.nicolai.sensmotiongruppe5.Fragments.Pie_chart_frag;
+import com.jaygoo.widget.RangeSeekBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,23 +20,26 @@ import java.util.List;
 public class Min_Data_Activity extends FragmentActivity {
 
     MyPageAdapter pageAdapter;
-
+    RangeSeekBar bar;
 
     @Override
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+            String[] haha = {"20:20","10:10","5:5","10:2"};
 
         setContentView(R.layout.activity_min__data);
         List<Fragment> fragments = getFragments();
 
-
+        bar = findViewById(R.id.Min_data_range_silder);
         pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
 
         ViewPager pager = findViewById(R.id.min_data_fragment_pager);
-
-
+        bar.setRange(1,10);
+        bar.setTickMarkMode(RangeSeekBar.TRICK_MARK_MODE_OTHER);
+        bar.setTickMarkTextArray(haha);
+        bar.setTickMarkTextColor(Color.parseColor("#03A9F4"));
         pager.setAdapter(pageAdapter);
 
 
