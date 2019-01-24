122e717459aeb5482e3c44312bc0b0ee46cbf1b2
Alan <s122417@student.dtu.dk>
2019-01-24 19:42:39 +0100

    added sound to win activity

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Win_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Win_Activity.java
@@ -1,6 +1,7 @@
 package com.example.nicolai.sensmotiongruppe5;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ public class Win_Activity extends AppCompatActivity implements View.OnClickListe
     private TextView textview;
     private Button share, goBack;
     int n;
+    MediaPlayer winSound;
 
     EmojiRainLayout mContainer;
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +46,9 @@ public class Win_Activity extends AppCompatActivity implements View.OnClickListe
 
 
 
+
+        winSound = MediaPlayer.create(this, R.raw.accelerate);
+        winSound.start();
 
 
             mContainer = findViewById(R.id.activity_win);
