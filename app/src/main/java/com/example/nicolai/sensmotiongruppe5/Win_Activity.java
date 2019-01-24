749db7b28c407dbcb219c47e9b15d6a033d7def3
s163312 <eliaspoulsen96@gmail.com>
2019-01-24 19:39:24 +0100

    Color changed in min_data slider

--- app/src/main/java/com/example/nicolai/sensmotiongruppe5/Win_Activity.java
+++ app/src/main/java/com/example/nicolai/sensmotiongruppe5/Win_Activity.java
@@ -2,6 +2,9 @@ package com.example.nicolai.sensmotiongruppe5;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
